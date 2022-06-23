597c6af164c20736c71360ada8d19b33604790a4
andrew <casal.andrew124@gmail.com>
2022-06-22 22:04:26 -0400

    34. created map method for TrackList component

--- jammming/src/Components/TrackList/TrackList.js
+++ jammming/src/Components/TrackList/TrackList.js
@@ -6,7 +6,11 @@ class TrackList extends React.Component {
     render() {
         return (
             <div className="TrackList">
-                <Track />
+                {
+                    this.props.tracks.map(track => {
+                        return <Track track={track} key={track.id} />
+                    })
+                }
             </div>
         )
     }
