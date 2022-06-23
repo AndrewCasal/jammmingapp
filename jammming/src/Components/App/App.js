40a9b5a96084120721d4e863d67af0e71cab4f7c
andrew <casal.andrew124@gmail.com>
2022-06-22 22:11:42 -0400

    37. hardcorded playlistName & playlistTracks

--- jammming/src/Components/App/App.js
+++ jammming/src/Components/App/App.js
@@ -9,7 +9,9 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      searchResults: [{name: 'name1', artist:'artist1', album: 'album1', id:1}, {name: 'name2', artist:'artist2', album: 'album2', id:2}, {name: 'name3', artist:'artist3', album: 'album3', id:3}]
+      searchResults: [{name: 'name1', artist:'artist1', album: 'album1', id:1}, {name: 'name2', artist:'artist2', album: 'album2', id:2}, {name: 'name3', artist:'artist3', album: 'album3', id:3}],
+      playlistName: 'My Playlist',
+      playlistTracks: [{name: 'playlistName1', artist:'playlistArtist1', album: 'playlistAlbum1', id:1}, {name: 'playlistName2', artist:'playlistArtist2', album: 'playlistAlbum2', id:2}, {name: 'playlistName3', artist:'playlistArtist3', album: 'playlistAlbum3', id:3}]
     }
   }
 
