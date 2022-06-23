eec461c42e12ba1de098104507ac79b07ce663d2
andrew <casal.andrew124@gmail.com>
2022-06-22 21:57:03 -0400

    corrected to props

--- jammming/src/Components/SearchResults/SearchResults.js
+++ jammming/src/Components/SearchResults/SearchResults.js
@@ -7,7 +7,7 @@ class SearchResults extends React.Component {
         return (
             <div className="SearchResults">
                 <h2>Results</h2>
-                <TrackList searchResults={this.state.searchResults} />
+                <TrackList searchResults={this.props.searchResults} />
             </div> 
         );
     }
