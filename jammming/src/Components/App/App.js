c538738b43b45f86dceee0bae9a965b1d7e7969d
andrew <casal.andrew124@gmail.com>
2022-06-22 21:11:13 -0400

    12. added components to app.js

--- jammming/src/Components/App/App.js
+++ jammming/src/Components/App/App.js
@@ -5,12 +5,12 @@ class App extends React.Component {
   render() {
     return (
       <div>
-      <h1>Ja<span class="highlight">mmm</span>ing</h1>
-        <div class="App">
-        <!-- Add a SearchBar component -->
-          <div class="App-playlist">
-            <!-- Add a SearchResults component -->
-            <!-- Add a Playlist component -->
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+        {/* <SearchBar /> */}
+          <div className="App-playlist">
+            {/* <SearchResults /> */}
+            {/* <Playlist /> */}
           </div>
         </div>
       </div>
