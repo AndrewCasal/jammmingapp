e2eda146d907144a34304cf3d02dbace541024af
andrew <casal.andrew124@gmail.com>
2022-06-22 21:09:27 -0400

    11. updated app class and return statement

--- jammming/src/Components/App/App.js
+++ jammming/src/Components/App/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1>Ja<span class="highlight">mmm</span>ing</h1>
+        <div class="App">
+        <!-- Add a SearchBar component -->
+          <div class="App-playlist">
+            <!-- Add a SearchResults component -->
+            <!-- Add a Playlist component -->
+          </div>
+        </div>
+      </div>
+      );
+    }
+  }
 
 export default App;
