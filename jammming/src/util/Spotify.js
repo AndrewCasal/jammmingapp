0b487a5ee7b03fc63c4ebdec08d7b389be2cae95
andrew <casal.andrew124@gmail.com>
2022-06-26 10:51:19 -0400

    80. checked for accessToken & expiration in URL

--- jammming/src/util/Spotify.js
+++ jammming/src/util/Spotify.js
@@ -7,8 +7,17 @@ const Spotify = {
             return userAccessToken
         }
 
-        const accessToken = window.location.href.match(/access_token=([^&]*)/);
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
         const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+
+            window.setTimeout(() => accessToken ='', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        }
     }
 }
 
