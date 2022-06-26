156581698b6bd351740297192389dc0578c5813e
andrew <casal.andrew124@gmail.com>
2022-06-26 10:31:20 -0400

    72. binded handleTermChange

--- jammming/src/Components/SearchBar/SearchBar.js
+++ jammming/src/Components/SearchBar/SearchBar.js
@@ -6,6 +6,7 @@ class SearchBar extends React.Component {
         super(props);
 
         this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
     }
     
     search() {
