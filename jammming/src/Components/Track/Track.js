d20635d2f3684d3846347184de7cd2bc0b082dbd
andrew <casal.andrew124@gmail.com>
2022-06-26 10:03:15 -0400

    53. created removeTrack

--- jammming/src/Components/Track/Track.js
+++ jammming/src/Components/Track/Track.js
@@ -20,6 +20,10 @@ class Track extends React.Component {
     addTrack() {
         this.props.onAdd(this.props.track);
     }
+
+    removeTrack() {
+        this.props.onRemove(this.props.track);
+    }
     
     render() {
         return (
