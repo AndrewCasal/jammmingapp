63d4a50bd943412212f9d307c72653be71ea6313
andrew <casal.andrew124@gmail.com>
2022-06-26 09:38:08 -0400

    41. created addTrack method

--- jammming/src/Components/App/App.js
+++ jammming/src/Components/App/App.js
@@ -15,6 +15,16 @@ class App extends React.Component {
     }
   }
 
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    }
+
+    tracks.push(track);
+    this.setState({playlistTracks: tracks});
+  }
+
   render() {
     return (
       <div>
