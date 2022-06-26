da56b5a62f6c1b980b4bcd59ce235c8a25b2d8d4
andrew <casal.andrew124@gmail.com>
2022-06-26 10:09:03 -0400

    58. binded updatePlaylistName and passed

--- jammming/src/Components/App/App.js
+++ jammming/src/Components/App/App.js
@@ -15,6 +15,7 @@ class App extends React.Component {
     }
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -46,7 +47,7 @@ class App extends React.Component {
         <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.state.addTrack} />
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.state.removeTrack} />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.state.removeTrack} onNameChange={this.state.updatePlaylistName} />
           </div>
         </div>
       </div>
