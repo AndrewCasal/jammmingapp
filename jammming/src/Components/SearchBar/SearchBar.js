f6c8bae62169dbd7b000ec68837d1de9b243c636
andrew <casal.andrew124@gmail.com>
2022-06-26 10:28:30 -0400

    70. created constructor and binded search

--- jammming/src/Components/SearchBar/SearchBar.js
+++ jammming/src/Components/SearchBar/SearchBar.js
@@ -2,6 +2,12 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.search = this.search.bind(this);
+    }
+    
     search() {
         this.props.onSearch(this.state.term);
     }
