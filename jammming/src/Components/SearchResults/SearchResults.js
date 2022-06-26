2fc953d0f6c03f82bf6d8dcf8dd617ac28677431
andrew <casal.andrew124@gmail.com>
2022-06-26 09:42:42 -0400

    43. passed onAdd to TrackList

--- jammming/src/Components/SearchResults/SearchResults.js
+++ jammming/src/Components/SearchResults/SearchResults.js
@@ -7,7 +7,7 @@ class SearchResults extends React.Component {
         return (
             <div className="SearchResults">
                 <h2>Results</h2>
-                <TrackList searchResults={this.props.searchResults} />
+                <TrackList searchResults={this.props.searchResults} onAdd={this.props.onAdd} isRemoval={false} />
             </div> 
         );
     }
