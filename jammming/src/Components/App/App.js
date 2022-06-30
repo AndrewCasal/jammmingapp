984d088bd68c1ab1aebd72f0b2b8b494ce97395b
andrew <casal.andrew124@gmail.com>
2022-06-29 22:38:04 -0400

    removed hardcoding of tracks

--- jammming/src/Components/App/App.js
+++ jammming/src/Components/App/App.js
@@ -10,9 +10,9 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      searchResults: [{name: 'name1', artist:'artist1', album: 'album1', id:1}, {name: 'name2', artist:'artist2', album: 'album2', id:2}, {name: 'name3', artist:'artist3', album: 'album3', id:3}],
+      searchResults: [],
       playlistName: 'My Playlist',
-      playlistTracks: [{name: 'playlistName1', artist:'playlistArtist1', album: 'playlistAlbum1', id:4}, {name: 'playlistName2', artist:'playlistArtist2', album: 'playlistAlbum2', id:5}, {name: 'playlistName3', artist:'playlistArtist3', album: 'playlistAlbum3', id:6}]
+      playlistTracks: []
     }
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
@@ -43,7 +43,6 @@ class App extends React.Component {
   }
 
   savePlaylist() {
-    alert("this method is linked");
     const trackUris = this.state.playlistTracks.map(track => track.uri);
     Spotify.savePlaylist(this.state.playlistName, trackUris).then(() => {
       this.setState({
@@ -55,7 +54,7 @@ class App extends React.Component {
 
   search(term) {
     Spotify.search(term).then(searchResults => {
-      this.setState({seachResults: searchResults})
+      this.setState({searchResults: searchResults})
     })
   }
 
