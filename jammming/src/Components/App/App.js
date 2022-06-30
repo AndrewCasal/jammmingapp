67889c30f40ac4a15490601c99535aa66a1c2a12
andrew <casal.andrew124@gmail.com>
2022-06-29 21:56:00 -0400

    fixed searchBar state

--- jammming/src/Components/App/App.js
+++ jammming/src/Components/App/App.js
@@ -64,7 +64,7 @@ class App extends React.Component {
       <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-        <SearchBar onSearch={this.state.search} />
+        <SearchBar onSearch={this.search} />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist} />
--- jammming/src/Components/SearchBar/SearchBar.js
+++ jammming/src/Components/SearchBar/SearchBar.js
@@ -5,6 +5,10 @@ class SearchBar extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            term: ''
+        }
+        
         this.search = this.search.bind(this);
         this.handleTermChange = this.handleTermChange.bind(this);
     }
