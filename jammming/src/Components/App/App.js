e57c13b78590e1223a663f8eac244373507158d6
andrew <casal.andrew124@gmail.com>
2022-08-30 12:52:51 -0400

    fixed trackCheck function

--- jammming/src/Components/App/App.js
+++ jammming/src/Components/App/App.js
@@ -62,10 +62,12 @@ class App extends React.Component {
   loadingPlaylist() {
     var saveButton = document.getElementsByClassName("Playlist-save");
     var saving = this.savePlaylist();
-    trackCheck = () => {
-      if(!this.playlistTracks.length) {
+    var savedTracks = this.playlistTracks.length;
+    const trackCheck = () => {
+      if(!savedTracks) {
         return alert('You got no tracks in your playlist dude!');
       }
+
     }
 
     saveButton.onClick()
