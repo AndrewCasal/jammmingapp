0b4b660608befa2aedd45435a17cf87a3c0b38ef
andrew <casal.andrew124@gmail.com>
2022-08-29 22:35:31 -0400

    started writing HTML for render

--- jammming/src/Components/LoadingScreen/LoadingScreen.js
+++ jammming/src/Components/LoadingScreen/LoadingScreen.js
@@ -9,8 +9,13 @@ class LoadingScreen extends React.Component {
         }
     }
     
+    // For the render need to figure out to get the playlistName in the comment //
+
     render() {
         return (
+            <div className="loading_box">
+                <h1>Your  is being Saved!</h1>
+            </div>
 
         );
     }
