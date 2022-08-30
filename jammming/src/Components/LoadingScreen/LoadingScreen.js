ec3bf35736bafaf77ccad02b6c155e365101f9d8
andrew <casal.andrew124@gmail.com>
2022-08-29 22:44:52 -0400

    removed method from component

--- jammming/src/Components/LoadingScreen/LoadingScreen.js
+++ jammming/src/Components/LoadingScreen/LoadingScreen.js
@@ -1,13 +1,6 @@
 import React from 'react';
 
 class LoadingScreen extends React.Component {
-    playlistStatus() {
-        if(playlistTracks.id) {
-
-        } else {
-            return alert('No songs saved to your new playlist!');
-        }
-    }
     
     // For the render need to figure out to get the playlistName in the comment //
 
@@ -16,7 +9,6 @@ class LoadingScreen extends React.Component {
             <div className="loading_box">
                 <h1>Your  is being Saved!</h1>
             </div>
-
         );
     }
 }
