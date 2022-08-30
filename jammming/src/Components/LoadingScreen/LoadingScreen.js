c77ad4621c371cf4fc20d40e6396372edda8d9ed
andrew <casal.andrew124@gmail.com>
2022-08-29 22:30:26 -0400

    Wrote playlistStatus method and else statement

--- jammming/src/Components/LoadingScreen/LoadingScreen.js
+++ jammming/src/Components/LoadingScreen/LoadingScreen.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 class LoadingScreen extends React.Component {
+    playlistStatus() {
+        if(playlistTracks.id) {
+
+        } else {
+            return alert('No songs saved to your new playlist!');
+        }
+    }
+    
     render() {
         return (
 
