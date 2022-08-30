3e114c5f3a621d0507700d611f047a4bc2517da5
andrew <casal.andrew124@gmail.com>
2022-08-30 13:17:23 -0400

    added highlight to LoadingScreen component

--- jammming/src/Components/LoadingScreen/LoadingScreen.js
+++ jammming/src/Components/LoadingScreen/LoadingScreen.js
@@ -4,7 +4,8 @@ class LoadingScreen extends React.Componenet {
     render() {
         return (
             <div className="loading_page">
-                <h1 className="loading_title">Your New Playlist is Saving!</h1>
+                <h1>Ja<span className="highlight">mmm</span>ing</h1>
+                <h2 className="loading_title">Your New Playlist is Saving!</h2>
             </div>
         );
     }
