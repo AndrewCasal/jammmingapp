e081dd7263604de5b8f8c9c2b351670af2b37284
andrew <casal.andrew124@gmail.com>
2022-08-30 13:25:00 -0400

    corrected React.Component

--- jammming/src/Components/LoadingScreen/LoadingScreen.js
+++ jammming/src/Components/LoadingScreen/LoadingScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-class LoadingScreen extends React.Componenet {
+class LoadingScreen extends React.Component {
     constructor(props) {
         super(props);
     }
     
+
     render() {
         return (
             <div className="loading_page">
