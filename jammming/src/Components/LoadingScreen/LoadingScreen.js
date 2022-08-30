27401bdf48baad67e9982a8b32bf5e6f2268b3cc
andrew <casal.andrew124@gmail.com>
2022-08-30 13:14:12 -0400

    added title to LoadingScreen component

--- jammming/src/Components/LoadingScreen/LoadingScreen.js
+++ jammming/src/Components/LoadingScreen/LoadingScreen.js
@@ -4,7 +4,7 @@ class LoadingScreen extends React.Componenet {
     render() {
         return (
             <div className="loading_page">
-
+                <h1 className="loading_title">Your New Playlist is Saving!</h1>
             </div>
         );
     }
