d484d3d39577790170185bd992c3dd12f4fa544e
andrew <casal.andrew124@gmail.com>
2022-09-01 13:00:09 -0400

    imported useState

--- jammming/src/Components/App/App.js
+++ jammming/src/Components/App/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
