1a440e2e9042de820a580b1f5cba3d0cbcbc0414
andrew <casal.andrew124@gmail.com>
2022-09-01 13:10:46 -0400

    imported useEffect

--- jammming/src/Components/App/App.js
+++ jammming/src/Components/App/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
